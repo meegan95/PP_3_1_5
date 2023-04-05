71a4c31d4f02085ae078c18df6d4b37319818b05
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-05 22:43:41 +0300

    admin panel 0.45

--- src/main/resources/static/show.js
+++ src/main/resources/static/show.js
@@ -1,28 +1,23 @@
 
-const tableUser = document.getElementById('tableUser')
+const tableOneUser = document.getElementById('tableOneUser')
 
 
-
-const url = 'http://localhost:8080/users'
+const urlForOne = 'http://localhost:8080/users/1'
 
 
 // $('#tableUser').empty()
-fetch(url)
+fetch(urlForOne)
     .then(response => response.json())
     .then(data => {
-        let columnElement = ''
-        data.forEach(userFromRequest => {
-            columnElement += `<tr>
-              <td>${userFromRequest.id}</td>
-              <td>${userFromRequest.firstName}</td>
-              <td>${userFromRequest.lastName}</td>
-              <td>${userFromRequest.age}</td>
-              <td>${userFromRequest.username}</td>
-              <td>${userFromRequest.role.map(role => role.name.substring(5))}</td>
+        let columnElement = `<tr>
+              <td>${data.id}</td>
+              <td>${data.firstName}</td>
+              <td>${data.lastName}</td>
+              <td>${data.age}</td>
+              <td>${data.username}</td>
+              <td>${data.role.map(role => role.name.substring(5))}</td>
             </tr>
            `
-            console.log(columnElement)
-        })
-        tableUser.innerHTML= columnElement
+        console.log(columnElement)
+        tableOneUser.innerHTML = columnElement
     })
-    // .catch(error => console.error(error))
