661bf2251c00e503bbfbd97df98a121a0e372a40
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-05 21:45:45 +0300

    admin panel 0.3

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,10 @@
 
 const tableUser = document.getElementById('tableAllUsers')
-
+const tableOneUser = document.getElementById('tableOneUser')
 
 
 const url = 'http://localhost:8080/users'
-
+const urlForOne = 'http://localhost:8080/users'
 
 // $('#tableUser').empty()
 fetch(url)
@@ -26,3 +26,24 @@ fetch(url)
         tableUser.innerHTML= columnElement
     })
 // .catch(error => console.error(error))
+
+
+
+fetch(urlForOne)
+    .then(response => response.json())
+    .then(data => {
+        let columnElement = ''
+        data.forEach(userFromRequest => {
+            columnElement += `<tr>
+              <td>${userFromRequest.id}</td>
+              <td>${userFromRequest.firstName}</td>
+              <td>${userFromRequest.lastName}</td>
+              <td>${userFromRequest.age}</td>
+              <td>${userFromRequest.username}</td>
+              <td>${userFromRequest.role.map(role => role.name.substring(5))}</td>
+            </tr>
+           `
+            console.log(columnElement)
+        })
+        tableOneUser.innerHTML= columnElement
+    })
