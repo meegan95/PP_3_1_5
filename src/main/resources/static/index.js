93853e14d27b4dfcd0ef36c7e6930750e731b533
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-05 23:12:45 +0300

    modal edit filled

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,6 @@
 const tableUser = document.getElementById('tableAllUsers')
 const tableOneUser = document.getElementById('tableOneUser')
-
+const modalEditButton = document.getElementById('modalEditButton')
 
 const url = 'http://localhost:8080/users'
 const urlForOne = 'http://localhost:8080/users/1'
@@ -55,5 +55,52 @@ fetch(urlForOne)
            `
         console.log(columnElement)
         tableOneUser.innerHTML = columnElement
+    })
+
+
+fetch(urlForOne)
+    .then(response => response.json())
+    .then(user => {
+        let columnElement = `
+        
+                   <input type="hidden" value="${user.id}" name="id">
+
+                    <p class="h6">First name</p>
+                   <input type="text" class=" mb-2 form-control" required
+                        name="firstName"
+                       value="${user.firstName}">
+
+                    <p class="h6">Last name</p>
+                    <input type="text" class="  mb-2 form-control" required
+                          name="lastName"
+                        value="${user.lastName}">
+
+                           <p class="h6">Age</p>
+                   <input type="number" class="mb-2 form-control" required
+                   name="age"
+                    value="${user.age}">
+
+                    <p class="h6 ">Email</p>
+                     <input type="email" class=" mb-2 form-control" required
+                     name="username"
+                     value="${user.username}">
+
+                     <p class="h6 ">Password</p>
+                     <input type="password" class=" mb-2 form-control"
+                     required
+                     name="password"
+                     value="${user.password}">
+
+                     <select id="editUserRoles" class="form-select" required
+                      name="roles" multiple size="2">
+                      <option
+                      value="${user.role.map(role => role.id)}"
+                      th:text="${user.role.map(role => role.name.substring(5))}" selected>
+                     </option>
+
+                     </select>
+           `
+        console.log(columnElement)
+        modalEditButton.innerHTML = columnElement
     })
 
