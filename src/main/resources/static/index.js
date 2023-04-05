f76948ad5e82ba27d5d721a355645c1ce2021dd0
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-05 16:19:17 +0300

    table filled more correctly

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,16 @@
-let tableAll = document.getElementById('tbodyAllUserTable')
+let columnId = document.getElementById('columnId')
+let columnFirstName = document.getElementById('columnFirstName')
+let columnLastName = document.getElementById('columnLastName')
+let columnAge = document.getElementById('columnAge')
+let columnUsername = document.getElementById('columnUsername')
+let columnRole = document.getElementById('columnRole')
 
+// <td id="columnId" th:text="id"></td>
+// <td id="columnFirstName" th:text="firstName"></td>
+// <td id="columnLastName" th:text="lastName"></td>
+// <td id="columnAge" th:text="age"></td>
+// <td id="columnUsername" th:text="username"></td>
+// <td id="columnRole"
 
  function fillTable(textContent, content) {
     textContent.textContent = content
@@ -10,18 +21,13 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
             console.log(data)
-            data.forEach(userFromData => {
-                    let filledTable = ` 
-                <tr> 
-                <td>${userFromData.id}</td>
-                <td>${userFromData.firstName}</td>
-                <td>${userFromData.lastName}</td>
-                <td>${userFromData.age}</td>
-                <td>${userFromData.username}</td>
-                <td>${userFromData.role}</td>
-                </tr>`;
-                tableAll.append(filledTable)
-
+            data.forEach(userFromRequest => {
+                fillTable(columnId,userFromRequest.id)
+                fillTable(columnFirstName,userFromRequest.firstName)
+                fillTable(columnLastName,userFromRequest.lastName)
+                fillTable(columnAge,userFromRequest.age)
+                fillTable(columnUsername,userFromRequest.username)
+                fillTable(columnRole,userFromRequest.role)
                 })
         })
     .catch(error => console.error(error))
