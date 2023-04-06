afe71b26d9fe1c3de080167fb28db4d828f669a7
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-06 13:28:45 +0300

    current filled table user panel

--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -1,23 +1,44 @@
 
 const tableOneUser = document.getElementById('tableOneUser')
+const currentUserLogin = document.getElementById('currentUserLogin')
 
-
-const urlForOne = 'http://localhost:8080/users/1'
+const url = 'http://localhost:8080/users'
 
 
 // $('#tableUser').empty()
-fetch(urlForOne)
+// fetch(urlForOne)
+//     .then(response => response.json())
+//     .then(data => {
+//         let columnElement = `<tr>
+//               <td>${data.id}</td>
+//               <td>${data.firstName}</td>
+//               <td>${data.lastName}</td>
+//               <td>${data.age}</td>
+//               <td>${data.username}</td>
+//               <td>${data.role.map(role => role.name.substring(5))}</td>
+//             </tr>
+//            `
+//         console.log(columnElement)
+//         tableOneUser.innerHTML = columnElement
+//     })
+
+
+fetch(url)
     .then(response => response.json())
     .then(data => {
-        let columnElement = `<tr>
-              <td>${data.id}</td>
-              <td>${data.firstName}</td>
-              <td>${data.lastName}</td>
-              <td>${data.age}</td>
-              <td>${data.username}</td>
-              <td>${data.role.map(role => role.name.substring(5))}</td>
+        let columnElement = ''
+        data.forEach(userFromRequest => {
+            if (userFromRequest.username === currentUserLogin.innerText)
+            columnElement += `<tr>
+              <td>${userFromRequest.id}</td>
+              <td>${userFromRequest.firstName}</td>
+              <td>${userFromRequest.lastName}</td>
+              <td>${userFromRequest.age}</td>
+              <td>${userFromRequest.username}</td>
+              <td>${userFromRequest.role.map(role => role.name.substring(5))}</td>
             </tr>
            `
-        console.log(columnElement)
+            console.log(columnElement)
+        })
         tableOneUser.innerHTML = columnElement
     })
